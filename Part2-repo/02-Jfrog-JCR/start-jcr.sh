4adb276d94564466ec5b5f877b42c023edca3c1f
Your Name <you@example.com>
2024-12-11 18:06:55 +0200

    add start/stop script

---
+++ Part2-repo/02-Jfrog-JCR/start-jcr.sh
@@ -0,0 +1,3 @@
+docker compose -p rt-postgres -f docker-compose-postgres.yaml up -d
+docker compose -p rt up -d
+
---
+++ Part2-repo/02-Jfrog-JCR/stop-jcr.sh
@@ -0,0 +1,5 @@
+
+
+docker compose -p rt-postgres -f docker-compose-postgres.yaml down
+docker compose -p rt down
+
